f98dd6eea14779d30a5b77ce02a0f6a4396be4ec
karekear <karekear@gmail.com>
2019-09-30 18:45:39 +0900

    jquery 共通化 失敗 敗北

---
+++ header.php
@@ -0,0 +1,19 @@
+
+
+<!-- header -->
+
+<h1>けあ(タイトル未定)</h1>
+
+<img src="shojo190916_wide.png">
+
+<ul class="menu">
+    <li class="menubutton"><a class="active" href="index.html">トップ</a></li>
+    <li class="menubutton"><a href="about.html">概要</a></li>
+    <li class="menubutton"><a href="preparation.html">少女</a></li>
+    <li class="menubutton"><a href="preparation.html">？？？</a></li>       
+    <li class="menubutton"><a href="preparation.html">コンテンツ</a></li>
+    <li class="menubutton"><a href="preparation.html">雑記</a></li>
+    <li class="menubutton"><a href="preparation.html">アクセス</a></li>
+</ul>
+
+<!-- header -->
