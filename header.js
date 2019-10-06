d6c7c4791d16d29d90b4fc7ba4745424c4710244
karekear <karekear@gmail.com>
2019-10-06 21:23:04 +0900

    少女の画像追加，共通部分JSで作成

---
+++ header.js
@@ -0,0 +1,16 @@
+header = 
+'<div id = "header">'+
+'<h1 class = "keah1">けあ研</h1>'+
+'<img src="shojo190916_wide.png">'+
+'<ul class="menu">'+
+'<li class="menubutton"><a class="active" href="index.html">トップ</a></li>'+
+'<li class="menubutton"><a href="about.html" target="_top">概要</a></li>'+
+'<li class="menubutton"><a href="shojo.html" target="_top">少女</a></li>'+
+'<li class="menubutton"><a href="preparation.html" target="_top">？？？</a></li>'+
+'<li class="menubutton"><a href="preparation.html" target="_top">コンテンツ</a></li>'+
+'<li class="menubutton"><a href="preparation.html" target="_top">雑記</a></li>'+
+'<li class="menubutton"><a href="preparation.html" target="_top">アクセス</a></li>'+
+'<li class="menubutton"><a href="q_and_a.html" target="_top">Ｑ＆Ａ</a></li>'+
+'</ul>'+
+'</div>';
+document.write(header);
