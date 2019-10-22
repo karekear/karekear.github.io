49eb6fe927fa5684d96b194d4a8054774eac22da
karekear <karekear@gmail.com>
2019-10-23 00:27:13 +0900

    made ???

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ header =
 '<ul class="menu">'+
 '<li class="menubutton"><a class="active" href="index.html">トップ</a></li>'+
 '<li class="menubutton"><a href="shojo.html" target="_top">少女</a></li>'+
-'<li class="menubutton"><a href="preparation.html" target="_top">？？？</a></li>'+
+'<li class="menubutton"><a href="what.html" target="_top">？？？</a></li>'+
 '<li class="menubutton"><a href="contents.html" target="_top">コンテンツ</a></li>'+
 '<li class="menubutton"><a href="zakki.html" target="_top">雑記</a></li>'+
 '<li class="menubutton"><a href="q_and_a.html" target="_top">Ｑ＆Ａ</a></li>'+
