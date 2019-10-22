1e47ff9faaeed8a6d7e6fac239d6595ed0b4cd2d
karekear <karekear@gmail.com>
2019-10-22 23:54:07 +0900

    久しぶりに更新(メニューボタン，少女，コンテンツ，雑記，ひとこと等)

--- header.js
+++ header.js
@@ -2,12 +2,10 @@ header =
 '<div id = "header">'+
 '<ul class="menu">'+
 '<li class="menubutton"><a class="active" href="index.html">トップ</a></li>'+
-'<li class="menubutton"><a href="about.html" target="_top">概要</a></li>'+
 '<li class="menubutton"><a href="shojo.html" target="_top">少女</a></li>'+
 '<li class="menubutton"><a href="preparation.html" target="_top">？？？</a></li>'+
-'<li class="menubutton"><a href="preparation.html" target="_top">コンテンツ</a></li>'+
-'<li class="menubutton"><a href="preparation.html" target="_top">雑記</a></li>'+
-'<li class="menubutton"><a href="preparation.html" target="_top">アクセス</a></li>'+
+'<li class="menubutton"><a href="contents.html" target="_top">コンテンツ</a></li>'+
+'<li class="menubutton"><a href="zakki.html" target="_top">雑記</a></li>'+
 '<li class="menubutton"><a href="q_and_a.html" target="_top">Ｑ＆Ａ</a></li>'+
 '</ul>'+
 '</div>';
